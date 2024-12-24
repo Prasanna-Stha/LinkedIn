5d6648f0d54aeb0882239b9bf262584c3724120b
Prasanna Shrestha <sthaprasanna131@gmail.com>
2024-12-24 16:52:48 +0545

    Removed Tailwind CDN and added local Tailwind configuration

--- src/components/LinkedIn/Navbar.jsx
+++ src/components/LinkedIn/Navbar.jsx
@@ -33,12 +33,12 @@ function Navbar({ profileSrc }) {
     <nav className="fixed top-0 w-full bg-gray-50 border-b border-gray-200 shadow-sm z-50">
       <div className="w-[90vw]  mx-auto flex justify-between items-center px-4 py-3">
         {/* Logo */}
-        <div className="flex items-center">
+        <div className="items-center hidden sm:flex">
           <img src={Logo} alt="LinkedIn Logo" className="h-10" />
         </div>
 
         {/* Search Box */}
-        <div className="hidden lg:flex relative w-[30%] md:w-[40%]">
+        <div className="hidden lg:flex relative w-[30%] sm:w-[100%] md:w-[40%]">
           <form className="flex items-center bg-gray-100 border border-gray-300 rounded-md px-3 py-1 w-full">
             <FontAwesomeIcon icon={faSearch} className="text-gray-500" />
             <input type="text" placeholder="Search for something..." className="hidden md:block bg-transparent focus:outline-none w-full text-sm pl-2 py-1" value={searchQuery} onChange={handleSearch} autoComplete="off"/>
